9bef3d7edad36c2dfe5ffdeca837cbb999944458
shravani0302 <parsishravzz@gmail.com>
2020-08-11 10:20:29 -0400

    Backend and frontend pieces of Amazona

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+export default
+{
+  products:[
+      {
+          _id:'1',
+          name:'Slim Shirt',
+          category:'Shirts',
+          image:'/images/d1.jpg',
+          price:60,
+          brand:'Nike',
+          rating:4.5,
+          numReviews:10,
+          countInStock:6,
+      },
+      {
+        _id:'2',
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:50,
+        brand:'Levis',
+        rating:4.1,
+        numReviews:7,
+        countInStock:5,
+    },
+    {
+        _id:'3',
+        name:'Fit Pants',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:70,
+        brand:'Jockey',
+        rating:4.4,
+        numReviews:8
+    },
+    {
+        _id:'4',
+        name:'Straight Pants',
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:90,
+        brand:'Levis',
+        rating:4.6,
+        numReviews:7
+    },
+    
+]
+}
